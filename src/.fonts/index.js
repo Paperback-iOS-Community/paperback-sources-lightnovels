e70bb45e1830d494cc5ea8a50c9f6720821270bc
phiefferj24 <71832633+phiefferj24@users.noreply.github.com>
2022-02-14 00:30:59 +0000

    Deploying to master from @ phiefferj24/paperback-sources-lightnovels@ca1649f7b1791e8e0ff4ac8d3f59a6339d4657a9 🚀

---
+++ src/.fonts/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fonts = void 0;
+const arial_1 = require("./arial");
+const timesnewroman_1 = require("./timesnewroman");
+const georgia_1 = require("./georgia");
+const sanfrancisco_1 = require("./sanfrancisco");
+exports.fonts = {
+    arial18: arial_1.arial.arial18,
+    arial24: arial_1.arial.arial24,
+    arial30: arial_1.arial.arial30,
+    arial36: arial_1.arial.arial36,
+    timesnewroman18: timesnewroman_1.timesnewroman.timesnewroman18,
+    timesnewroman24: timesnewroman_1.timesnewroman.timesnewroman24,
+    timesnewroman30: timesnewroman_1.timesnewroman.timesnewroman30,
+    timesnewroman36: timesnewroman_1.timesnewroman.timesnewroman36,
+    georgia18: georgia_1.georgia.georgia18,
+    georgia24: georgia_1.georgia.georgia24,
+    georgia30: georgia_1.georgia.georgia30,
+    georgia36: georgia_1.georgia.georgia36,
+    sanfrancisco18: sanfrancisco_1.sanfrancisco.sanfrancisco18,
+    sanfrancisco24: sanfrancisco_1.sanfrancisco.sanfrancisco24,
+    sanfrancisco30: sanfrancisco_1.sanfrancisco.sanfrancisco30,
+    sanfrancisco36: sanfrancisco_1.sanfrancisco.sanfrancisco36,
+};
